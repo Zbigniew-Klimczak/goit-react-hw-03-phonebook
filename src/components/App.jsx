0a7ea895d9b2b2f692c1ce03983ceddbd007d6d3
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-03-26 13:26:35 +0200

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Filter from './Filter';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import css from './App.module.css';
+export class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+  filterValueChange = evt => {
+    this.setState({ filter: evt.currentTarget.value.trim() });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const keyword = filter.toLowerCase();
+    const filteredContacts = contacts.filter(
+      contact => contact.name.toLowerCase().indexOf(keyword) > -1
+    );
+    return filteredContacts;
+  };
+  addContact = contact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+  deleteContact = contactToDelete => {
+    const { contacts } = this.state;
+    let newContacts = contacts.filter(contact => contact !== contactToDelete);
+    this.setState({ contacts: newContacts });
+  };
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <section className={css.phonebook}>
+        <h1 className={css.phonebookTitle}>Phonebook</h1>
+        <ContactForm
+          contacts={contacts}
+          addContact={this.addContact}
+        ></ContactForm>
+        <h2 className={css.contactsTitle}>Contacts</h2>
+        <Filter filterValue={filter} onChange={this.filterValueChange}></Filter>
+        <ContactList
+          deleteContact={this.deleteContact}
+          filteredContacts={this.filteredContacts}
+        ></ContactList>
+      </section>
+    );
+  }
+}
